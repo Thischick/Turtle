86e160e9b8d32a0c2545588dc51293e683d83638
Thischick <74176217+Thischick@users.noreply.github.com>
2021-01-24 08:14:17 +0330

    Add files via upload

---
+++ Turtle2.py
@@ -0,0 +1,16 @@
+from turtle import*
+
+bgcolor("black")
+speed(0)
+penup()
+goto(-200,0)
+pendown()
+for i in range (3):
+    for colours in ["red", "blue", "cyan", "green", "yellow", "white",]:
+        color(colours)
+        pensize(3)
+        circle(150)
+        forward(20)
+        left(90)
+
+        
